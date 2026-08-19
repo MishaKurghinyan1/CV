f404f79431061348c0479dbb0dd4caaf06c04c9d
MishaKurghinyan1 <minecon291@gmail.com>
2026-08-19 20:43:03 +0400

    fix: make hamburger menu disappear if it was open when resizing the width of the window

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,11 @@ const hamburger = document.getElementById("hamburger");
 const hamburgerMenu = document.querySelector(".hamburger-component");
 const closeBtn = document.getElementById("close");
 
+window.addEventListener("resize", (e) => {
+  e.preventDefault();
+  hamburgerMenu.classList.remove("active");
+});
+
 closeBtn.addEventListener("click", (e) => {
   e.preventDefault();
   hamburgerMenu.classList.remove("active");
